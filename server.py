54e6d36338144cd87a792e2e9c37328fa9099a76
Diana Mayorova <dianadukai@yandex.ru>
2026-09-20 13:06:49 +0300

    Полностью готова страницы контактов, главная, каталог bootstrap и сервер python с виртуальным окружением

--- server.py
+++ server.py
@@ -8,36 +8,45 @@ PORT = 8000
 class HomeworkServer(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        """Обработка всех входящих GET-запросов"""
+        """Обработка входящих GET-запросов"""
 
-        # Если браузер запрашивает файл из папки /css/
+        # 1. Отдача CSS-стилей
         if self.path.startswith("/css/"):
             try:
-                # Убираем первый слэш, чтобы получить путь 'css/bootstrap.min.css'
                 file_path = self.path.lstrip("/")
                 with open(file_path, "rb") as file:
                     css_content = file.read()
-
                 self.send_response(200)
                 self.send_header("Content-Type", "text/css")
                 self.end_headers()
                 self.wfile.write(css_content)
                 return
             except FileNotFoundError:
-                self.send_error(404, "File Not Found")
+                self.send_error(404, "CSS Not Found")
                 return
 
-        # Для всех остальных запросов отдаем HTML
+        # 2. Роутинг (выбор HTML-файла в зависимости от адреса в браузере)
+        if self.path == "/" or self.path == "":
+            target_html = "index.html"
+        elif self.path == "/catalog":
+            target_html = "catalog.html"
+        elif self.path == "/contacts":
+            target_html = "contacts.html"
+        else:
+            self.send_error(404, "Страница не найдена")
+            return
+
+        # 3. Отправка выбранного HTML-файла в браузер
         self.send_response(200)
         self.send_header("Content-Type", "text/html; charset=utf-8")
         self.end_headers()
 
         try:
-            with open("contacts.html", "rb") as file:
+            with open(target_html, "rb") as file:
                 html_content = file.read()
             self.wfile.write(html_content)
         except FileNotFoundError:
-            self.wfile.write(bytes("<h1>Файл contacts.html не найден!</h1>", "utf-8"))
+            self.wfile.write(bytes(f"<h1>Ошибка: файл {target_html} не найден!</h1>", "utf-8"))
 
     def do_POST(self):
         """Метод do_POST срабатывает, когда пользователь нажимает кнопку 'Отправить' в форме"""
